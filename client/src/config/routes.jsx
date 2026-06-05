2fec14272a1291829f56a70498f66961cc68bf0f
Eduardo <eduman.000@gmail.com>
2026-06-05 11:17:52 -0300

    Reestructura sección maquinarias

--- client/src/config/routes.jsx
+++ client/src/config/routes.jsx
@@ -2,6 +2,7 @@ import HomePage from '../pages/HomePage.jsx';
 import AboutPage from '../pages/AboutPage.jsx';
 import ContactPage from '../pages/ContactPage.jsx';
 import MachinesPage from '../pages/MachinesPage.jsx';
+import MachinesDetailPage from '../pages/MachinesDetailPage.jsx';
 import ServicesPage from '../pages/ServicesPage.jsx';
 import SparePartDetailPage from '../pages/SparePartDetailPage.jsx';
 import SparePartsPage from '../pages/SparePartsPage.jsx';
@@ -11,6 +12,7 @@ export const routes = {
   '/repuestos': SparePartsPage,
   '/repuestos/:id': SparePartDetailPage,
   '/maquinarias': MachinesPage,
+  '/maquinarias/:id': MachinesDetailPage,
   '/servicios': ServicesPage,
   '/acerca-de': AboutPage,
   '/contacto': ContactPage
